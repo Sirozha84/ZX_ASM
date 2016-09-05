2a8877608e9c01610f6d23f661b2697edcf963d0
Сергей Гордеев <sirozhagordeev@gmail.com>
2016-09-05 08:41:02 +0700

    Команды сложения и вычитания

--- Token.cs
+++ Token.cs
@@ -47,19 +47,19 @@ namespace ZXASM
                     {
                         if (Str[1] == "a")
                         {
-                            if (ReadAdr(Str[2], out NN)) { Code = new byte[] { 58, B1(NN), B2(NN) }; };  //LD A,(NN)
+                            if (ReadAdr(Str[2], out NN)) { Code = new byte[] { 58, B1(NN), B2(NN) }; };         //LD A,(NN)
                         }
                         if (Str[1] == "b")
                         {
-                            if (Str[2] == "b") Code = new byte[] { 64 };
-                            if (Str[2] == "c") Code = new byte[] { 65 };
-                            if (Str[2] == "d") Code = new byte[] { 66 };
-                            if (Str[2] == "e") Code = new byte[] { 67 };
-                            if (Str[2] == "h") Code = new byte[] { 68 };
-                            if (Str[2] == "l") Code = new byte[] { 69 };
-                            if (Str[2] == "(hl)") Code = new byte[] { 70 };
-                            if (Str[2] == "a") Code = new byte[] { 71 };
-                            if (ReadNum(Str[2], out NN)) { Code = new byte[] { 6, B1(NN) }; };  //LD B,NN
+                            if (ReadNum(Str[2], out NN)) { Code = new byte[] { 6, B1(NN) }; };                  //LD B,NN
+                            if (Str[2] == "b") Code = new byte[] { 64 };                                        //AD B,B
+                            if (Str[2] == "c") Code = new byte[] { 65 };                                        //LD B,C
+                            if (Str[2] == "d") Code = new byte[] { 66 };                                        //LD B,D
+                            if (Str[2] == "e") Code = new byte[] { 67 };                                        //LD B,E
+                            if (Str[2] == "h") Code = new byte[] { 68 };                                        //LD B,H
+                            if (Str[2] == "l") Code = new byte[] { 69 };                                        //LD B,L
+                            if (Str[2] == "(hl)") Code = new byte[] { 70 };                                     //LD B,(HL)
+                            if (Str[2] == "a") Code = new byte[] { 71 };                                        //LD B,A
                         }
                         if (Str[1] == "hl")
                         {
@@ -67,9 +67,47 @@ namespace ZXASM
                         }
                     }
                     break;
+                case "exx": Code = new byte[] { 217 }; break;
+                case "inc":
+                    if (Str.Count() == 2)
+                    {
+                        if (Str[1] == "b") Code = new byte[] { 4 };                                             //INC B
+                        if (Str[1] == "a") Code = new byte[] { 60 };                                            //INC A
+                    }
+                    break;
+                case "dec":
+                    if (Str.Count() == 2)
+                    {
+                        if (Str[1] == "b") Code = new byte[] { 5 };                                             //DEC B
+                        if (Str[1] == "a") Code = new byte[] { 61 };                                            //DEC A
+                    }
+                    break;
+                case "add":
+                    if (Str.Count() == 3)
+                    {
+                        if (Str[1] == "a")
+                        {
+                            if (Str[2] == "b") Code = new byte[] { 128 };                                       //ADD A,B
+                            if (Str[2] == "c") Code = new byte[] { 129 };                                       //ADD A,C
+                            if (Str[2] == "a") Code = new byte[] { 135 };                                       //ADD A,A
+                            if (ReadNum(Str[2], out NN)) { Code = new byte[] { 198, B1(NN) }; };                //ADD A,N
+                        }
+                    }
+                    break;
+                case "adc":
+                    if (Str.Count() == 3)
+                    {
+                        if (Str[1] == "a")
+                        {
+                            if (Str[2] == "b") Code = new byte[] { 136 };                                       //ADC A,B
+                            if (Str[2] == "c") Code = new byte[] { 137 };                                       //ADC A,C
+                            if (Str[2] == "a") Code = new byte[] { 143 };                                       //ADC A,A
+                            if (ReadNum(Str[2], out NN)) { Code = new byte[] { 198, B1(NN) }; };                //ADC A,N
+                        }
+                    }
+                    break;
                 case "nop": Code = new byte[] { 0 }; break;
                 case "ret": Code = new byte[] { 201 }; break;
-                case "exx": Code = new byte[] { 217 }; break;
                 case "reti": Code = new byte[] { 239, 77 }; break;
                 case "di": Code = new byte[] { 243 }; break;
                 case "ei": Code = new byte[] { 251 }; break;
