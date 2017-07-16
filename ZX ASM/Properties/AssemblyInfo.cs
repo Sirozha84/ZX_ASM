2f9b1c2755c28c78b6f061e04c51cb8609feb6b3
Сергей Гордеев <sirozhagordeev@gmail.com>
2017-07-16 12:27:24 +0700

    Полное открытие

---
+++ ZX ASM/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ZX ASM")]
+[assembly: AssemblyDescription("Компилятор языка Assembler для компьютера ZX Spectrum")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SG Software")]
+[assembly: AssemblyProduct("ZX ASM")]
+[assembly: AssemblyCopyright("Copyright © SG Software 2016")]
+[assembly: AssemblyTrademark("ZX ASM")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("978a2375-edeb-4efc-8cc6-dc1a736eeff9")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
